f8b775dbc0134bcaedaea009e39caedb86c79179
kfarooq1981 <148969272+kfarooq1981@users.noreply.github.com>
2026-04-08 20:32:03 +0500

    Initial commit from Flare Codegen (API)

---
+++ app/checkout/dine-in/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar';
+import DineInCheckout from '@/components/DineInCheckout';
+import Footer from '@/components/Footer';
+
+export default function DineInCheckoutPage() {
+  return (
+    <main>
+      <Navbar />
+      <DineInCheckout />
+      <Footer />
+    </main>
+  );
+}
