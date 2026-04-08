80ab6bb2061fee02e2ff4303804c899dd7dc0df0
kfarooq1981 <148969272+kfarooq1981@users.noreply.github.com>
2026-04-08 20:32:04 +0500

    Initial commit from Flare Codegen (API)

---
+++ app/checkout/delivery/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar';
+import DeliveryCheckout from '@/components/DeliveryCheckout';
+import Footer from '@/components/Footer';
+
+export default function DeliveryCheckoutPage() {
+  return (
+    <main>
+      <Navbar />
+      <DeliveryCheckout />
+      <Footer />
+    </main>
+  );
+}
